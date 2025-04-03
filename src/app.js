c37477e638086e21e2ea0fd138a0ef7dbc349574
carlossb21 <carlosandresb21@gmail.com>
2025-04-03 00:26:20 +0000

    cambios tarjeta

--- src/app.js
+++ src/app.js
@@ -5,7 +5,43 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
+function mostrarCarta(palo,valor,color){
+
+  let carta = ` <div class="card">
+                    <div class="superior" style="color:${color};">
+                      ${palo}
+                    </div>
+                    <div class="central">
+                      ${valor}
+                    </div>
+                    <div class="inferior" style="color:${color};">
+                      ${palo}
+                    </div>
+                  </div>`
+
+  document.querySelector('#content-main').innerHTML = carta;
+
+  let info = document.querySelector('#content-main');
+
+  console.log(info);
+
+}
+
+
+//evento carga de pagina
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+
+  let palos = ['♠', '♥', '♦', '♣']; // Picas, Corazones, Diamantes, Tréboles
+  let valores = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  let colores = ['red','black']
+
+  let posicionValorAleatorio = Math.floor(Math.random() * valores.length);
+  let posicionPaloAleatorio = Math.floor(Math.random() * palos.length);
+  let posicionColorAleatorio = Math.floor(Math.random() * colores.length);
+  mostrarCarta(palos[posicionPaloAleatorio],valores[posicionValorAleatorio],colores[posicionColorAleatorio]);
+
 };
+
+

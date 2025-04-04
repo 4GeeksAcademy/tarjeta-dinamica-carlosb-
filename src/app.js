b07216857b41f49a7c7144e8083d90502cc5e172
carlossb21 <carlosandresb21@gmail.com>
2025-04-04 02:44:26 +0000

    boton.mas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 
+let palos = ['♠', '♥', '♦', '♣']; // Picas, Corazones, Diamantes, Tréboles
+let valores = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+let colores = ['red','black']
+
+
 function mostrarCarta(palo,valor,color){
 
   let carta = ` <div class="card">
@@ -21,7 +26,6 @@ function mostrarCarta(palo,valor,color){
                   </div>`
 
   document.querySelector('#content-main').innerHTML = carta;
-
   let info = document.querySelector('#content-main');
 
   console.log(info);
@@ -31,12 +35,6 @@ function mostrarCarta(palo,valor,color){
 
 //evento carga de pagina
 window.onload = function() {
-
-
-  let palos = ['♠', '♥', '♦', '♣']; // Picas, Corazones, Diamantes, Tréboles
-  let valores = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-  let colores = ['red','black']
-
   let posicionValorAleatorio = Math.floor(Math.random() * valores.length);
   let posicionPaloAleatorio = Math.floor(Math.random() * palos.length);
   let posicionColorAleatorio = Math.floor(Math.random() * colores.length);
@@ -45,3 +43,38 @@ window.onload = function() {
 };
 
 
+
+//evento nuevo js
+/*
+document.getElementById('btnNuevaCarta').onclick = function(){
+  console.log('dio click, evento forma 1');
+}
+  */
+
+
+document.getElementById('btnNuevaCarta').addEventListener('click', function() {
+  let posicionValorAleatorio = Math.floor(Math.random() * valores.length);
+  let posicionPaloAleatorio = Math.floor(Math.random() * palos.length);
+  let posicionColorAleatorio = Math.floor(Math.random() * colores.length);
+  mostrarCarta(palos[posicionPaloAleatorio],valores[posicionValorAleatorio],colores[posicionColorAleatorio]);
+});
+
+document.getElementById('btnTamanio').addEventListener('click', function(){
+  let ancho = document.getElementById('valueWidth').value;
+  let alto = document.getElementById('valueHeight').value;
+
+  if(ancho != '' && alto != ''){
+    const cards = document.querySelectorAll('.card');
+
+    // Itera sobre todos los elementos encontrados
+    cards.forEach((card, index) => {
+        card.style.width = ancho + 'px';
+        card.style.height = alto + 'px';
+    });
+  }
+  
+
+
+  console.log('ancho ',ancho);
+  console.log('alto ',alto);
+});
